e9205d10b974c010da550e33f1334c326b2b0135
StanislasRo <stanislas.rouault@gmail.com>
2023-01-22 15:40:03 +0100

    commit4

--- app/db/model.py
+++ app/db/model.py
@@ -9,7 +9,7 @@ class Package(db.Model):
     __tablename__ = 'package'
     id = db.Column(db.Integer, primary_key=True)    
     destination = db.Column(db.String(length=100))
-    statut = db.Column(db.Integer, primary_key=True)  
+    statut = db.Column(db.String, primary_key=True)  
 
 class Warehouse(db.Model):
     __tablename__ = 'warehouse'
@@ -19,17 +19,18 @@ class Warehouse(db.Model):
 class SessionPrimary(db.Model):
     __tablename__ = 'session_primary'
     id = db.Column(db.Integer, primary_key=True)    
-    queue_name = db.Column(db.String(length=100))
-    tracker = db.Column(db.Integer, db.ForeignKey('tracker.id'))
     package = db.Column(db.Integer, db.ForeignKey('package.id'))
+    tracker = db.Column(db.Integer, db.ForeignKey('tracker.id'))
     warehouse = db.Column(db.Integer, db.ForeignKey('warehouse.id'))
+    statut = db.Column(db.String, primary_key=True)  
+    time = db.Column(db.Time(timezone=True))
 
 class SessionSecondary(db.Model):
     __tablename__ = 'session_secondary'
     id = db.Column(db.Integer, primary_key=True)    
-    queue_name = db.Column(db.String(length=100))
     package = db.Column(db.Integer, db.ForeignKey('package.id'))
     delivery_man = db.Column(db.Integer, db.ForeignKey('delivery_man.id'))
+    time = db.Column(db.Time(timezone=True))
 
 class DeliveryMan(db.Model):
     __tablename__ = 'delivery_man'
