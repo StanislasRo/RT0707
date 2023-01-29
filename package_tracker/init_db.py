6aeff1e7797a8316a639908bdae1d91b03b965b9
StanislasRo <stanislas.rouault@gmail.com>
2023-01-29 02:13:08 +0100

    htmljs

--- package_tracker/init_db.py
+++ package_tracker/init_db.py
@@ -11,29 +11,25 @@ def clean_mariadb():
     db.session.query(Warehouse).delete()
     db.session.commit()
 
-    
-def add_package():
-    print("add packages")
-    db.session.add(Package(destination='Nantes', status='new', tracking_number='10000'))
-    db.session.add(Package(destination='Rennes', status='in delivering', tracking_number='10001'))
-    db.session.add(Package(destination='Reims', status='delivered', tracking_number='10002'))
-    db.session.commit()
-
-def add_warehouse():
-    print("add warehouses")
-    db.session.add(Warehouse(name='warehouse one'))
-    db.session.add(Warehouse(name='warehouse two'))
-    db.session.add(Warehouse(name='warehouse three'))
-    db.session.commit()
-
-def add_delivery_man():
-    db.session.add(DeliveryMan(smartphone='1'))
-    db.session.add(DeliveryMan(smartphone='2'))
-    db.session.add(DeliveryMan(smartphone='3'))
-    db.session.commit()
-
-def add_smartphone():
-    db.session.add(Smartphone(latitude='47.21476', longitude='-1.55589'))
-    db.session.add(Smartphone(latitude='48.11208', longitude='-1.68376'))
-    db.session.add(Smartphone(latitude='49.24343', longitude='4.06231'))
+def add_db():
+    trackerone = Tracker(status='busy')
+    trackertwo = Tracker(status='busy')
+    trackerthree = Tracker(status='busy')
+    packageone = Package(destination='Nantes', status='new', tracking_number='10000')
+    packagetwo = Package(destination='Rennes', status='in delivering', tracking_number='10001')
+    packagethree = Package(destination='Reims', status='delivered', tracking_number='10002')
+    warehouseone = Warehouse(name='warehouse one')
+    warehousetwo = Warehouse(name='warehouse one')
+    warehousethree = Warehouse(name='warehouse one')
+    smartphoneone = Smartphone(latitude='47.21476', longitude='-1.55589')
+    smartphonetwo = Smartphone(latitude='48.11208', longitude='-1.68376')
+    smartphonethree = Smartphone(latitude='49.24343', longitude='4.06231')
+    deliverymanone = DeliveryMan(smartphone=smartphoneone)
+    deliverymantwo = DeliveryMan(smartphone=smartphonetwo)
+    deliverymanthree = DeliveryMan(smartphone=smartphonethree)
+    db.session.add_all([trackerone,trackertwo,trackerthree])
+    db.session.add_all([packageone,packagetwo,packagethree])
+    db.session.add_all([warehouseone,warehousetwo,warehousethree])
+    db.session.add_all([deliverymanone,deliverymantwo,deliverymanthree])
+    db.session.add_all([smartphoneone,smartphonetwo,smartphonethree])
     db.session.commit()
---
+++ package_tracker/static/js/tracking.js
@@ -0,0 +1,27 @@
+$(function() {
+    console.log( "ready!" );
+
+    $( "#form-tracking" ).submit(function( event ) {
+        event.preventDefault();
+        console.log($("#tracking-number").val());
+        $.ajax({
+            type: "GET", 	        //Méthode à employer POST ou GET 
+            url: "/api/v1/package?tracking-number="+$("#tracking-number").val(),  //Cible du script coté serveur à appeler
+        }).done(function (output) {
+            //Code à jouer en cas d'éxécution sans erreur du script du PHP
+            // Retrieve the package data from the database
+            var package_data = Package.query.filter_by(tracking_number=output).first()
+            // Display the package status and time on the page
+            $('#package_status').text(package_data.status)
+            $('#package_time').text(package_data.time)
+        }).fail(function (error) {
+            //Code à jouer en cas d'éxécution en erreur du script du PHP ou de ressource introuvable
+            var errorMessage = "Error: " + error;
+            console.log(errorMessage);
+            alert(errorMessage);
+        }).always(function () {
+        //Code à jouer après done OU fail quoi qu'il arrive
+        });
+    });
+
+});
